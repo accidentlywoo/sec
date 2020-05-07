f6a319882259485b53138f611ee8887ef0e042cd
accidentlywoo <accidentlywoo@gmail.com>
2020-05-07 17:42:09 +0900

    real dynamic dispatch practice

--- oop/src/dispatch/DoubleDispatch.java
+++ oop/src/dispatch/DoubleDispatch.java
@@ -5,35 +5,54 @@ import java.util.List;
 
 public class DoubleDispatch {
 	interface Post{
-		void postOn(Facebook sns);
-		void postOn(Twitter sns);
+		void postOn(SNS sns);
 		}
 	static class Text implements Post{
-		public void postOn(Facebook sns) {
-			System.out.println("text -> facebook");
-		}
-		public void postOn(Twitter sns) {
-			System.out.println("text -> twitter");
+		public void postOn(SNS sns) {
+			sns.post(this);
 		}
 	}
 	static class Picture implements Post{
-		public void postOn(Facebook sns) {
-			System.out.println("text -> facebook");
-		}
-		public void postOn(Twitter sns) {
-			System.out.println("text -> twitter");
+		public void postOn(SNS sns) {
+			sns.post(this);
 		}
 	}
-	interface SNS{}
-	static class Facebook implements SNS{};
-	static class Twitter implements SNS{};
+	interface SNS{
+		void post(Text post);
+		void post(Picture post);
+	}
+	static class Facebook implements SNS{
+		public void post(Text post) {
+			System.out.println("text -> facebook");
+		}
+		public void post(Picture post) {
+			System.out.println("post -> facebook");
+		}
+	};
+	static class Twitter implements SNS{
+		public void post(Text post) {
+			System.out.println("text -> Twitter");
+		}
+		public void post(Picture post) {
+			System.out.println("post -> Twitter");
+		}
+	};
+	static class GooglePlus implements SNS{
+		public void post(Text post) {
+			System.out.println("text -> GooglePlus");
+		}
+		public void post(Picture post) {
+			System.out.println("post -> GooglePlus");
+		}
+	};
 	
 	public static void main(String[] args) {
 		List<Post> posts = Arrays.asList(new Text(),new Picture());
-		List<SNS> snss = Arrays.asList(new Facebook(), new Twitter());
+		List<SNS> snss = Arrays.asList(new Facebook(), new Twitter(), new GooglePlus());
 	
-		// 컴파일 시점에서 매개변수 타입체크에 걸려서 더블 디스패칭을 할 수 없다.
-		// Override 되지 않고, Overload를 사용했기 때문에!!!!
+		/*
+		 * 
+		 */
 		posts.forEach(p->snss.forEach((SNS s)->p.postOn(s)));
 	}
 }
