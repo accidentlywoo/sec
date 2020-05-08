53257990533947ab8604cd44a4db1c809931eb8e
accidentlywoo <accidentlywoo@gmail.com>
2020-05-08 17:39:36 +0900

    짬나서 하는 알고리즘

--- oop/src/algorithm/StringToInt.java
+++ oop/src/algorithm/StringToInt.java
@@ -2,11 +2,27 @@ package algorithm;
 
 public class StringToInt {
 	public int solution(String s) {
+		char flag = ' ';
+		if(s.charAt(0) == '-') {
+			flag =  '-';
+			s = s.substring(1);
+		}
+		else if(s.charAt(0) == '+') {
+			flag =  '+';
+			s = s.substring(1);
+		}
+		
 	    try {
 			int answer =  Integer.parseInt(s);
+			if(flag!=' ')
+				answer = Integer.parseInt(flag+s);
 			return answer;
 		} catch (NumberFormatException  e) {
-			return 0;
+			return 0;// 숫자 변경 불가 문자열 입력
 		}
     }
+	public static void main(String[] args) {
+		StringToInt s = new StringToInt();
+		System.out.println(s.solution("-12"));
+	}
 }
