7d2a20a5221726d8dcad3988be3167e3fab5e07b
accidentlywoo <accidentlywoo@gmail.com>
2020-05-12 16:45:15 +0900

    service 기능 구현 practice login기능의 안좋은 예

--- oop/src/com/my/service/CustomerService.java
+++ oop/src/com/my/service/CustomerService.java
@@ -1,5 +1,80 @@
 package com.my.service;
 
-public class CustomerService {
+import com.my.dao.CustomerDAO;
+import com.my.dao.CustomerDAOArray;
+import com.my.exception.AddException;
+import com.my.exception.FindException;
+import com.my.vo.Customer;
 
+public class CustomerService {
+	private CustomerDAO customerDAO;
+	public CustomerService() {
+		customerDAO = new CustomerDAOArray();
+	}
+	public CustomerService(CustomerDAO customerDAO) {
+		this.customerDAO = customerDAO;
+	}
+	public CustomerDAO getCustomerDAO() {
+		return customerDAO;
+	}
+	public void setCustomerDAO(CustomerDAO customerDAO) {
+		this.customerDAO = customerDAO;
+	}
+	// 기능 구현 시작
+	public void add(Customer customer) throws AddException{
+		customerDAO.insert(customer);
+	}
+	
+	public Customer[] findAll() throws FindException{
+		return customerDAO.selectAll();
+	}
+	
+	public Customer findById(String id) throws FindException{ 
+		return customerDAO.selectById(id);
+	}
+
+	public Customer[] findByName(String name) throws FindException{ 
+		return null;
+	}
+	
+	public void login(String id, String pwd) throws FindException{
+		Customer customer = customerDAO.selectById(id);
+		if(!customer.getPwd().equals(pwd)) {
+			throw new FindException("비밀번호가 일치하지 않습니다.");
+		}
+		System.out.println("로그인 성공");
+	}
+	public static void main(String[] args) {
+		CustomerService customerService = new CustomerService();
+		try {
+			customerService.findAll();
+		} catch (FindException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			customerService.add(new Customer("id1", "p1","n1","a1"));
+		} catch (AddException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			Customer result = customerService.findById("id1");
+			System.out.println(result);
+		} catch (FindException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			customerService.login("id1", "p1");
+		} catch (FindException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			customerService.login("idㅁㅁㅁㅁ", "p1");
+		} catch (FindException e) {
+			e.printStackTrace();
+		}
+	}
 }
