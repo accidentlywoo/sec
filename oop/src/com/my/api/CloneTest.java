699f2856bc051e4ce27c3853812fca5235a22bbe
accidentlywoo <accidentlywoo@gmail.com>
2020-05-13 10:23:48 +0900

    Object class는 shallow copy만 제공하기 때문에 deepcopy를 직접 구현해야 한다

--- oop/src/com/my/api/CloneTest.java
+++ oop/src/com/my/api/CloneTest.java
@@ -1,5 +1,7 @@
 package com.my.api;
 
+import java.util.Arrays;
+
 class A implements Cloneable{
 	int i = 1;
 	int[] arr = {1, 2, 3};
@@ -7,6 +9,18 @@ class A implements Cloneable{
 		try {
 			Object object = this.clone();
 			return (A)object;
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public A deepCopy() {
+		try {
+			Object object = this.clone(); // shallow copy
+			int[] arr = (int[])this.arr.clone(); // reference type의 객체를 계속 shallow로 타야하나?
+			A a = (A) object;
+			a.arr = arr;
+			return a;
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
@@ -20,5 +34,23 @@ public class CloneTest {
 		A a1 = new A();
 		//a1.clone(); The method clone() from the type Object is not visible
 		A a2 = a1.shallowCopy();
+		
+		A a3 = a1.deepCopy();
+		
+		System.out.println(a1.hashCode());
+		System.out.println(a2.hashCode());
+		
+		System.out.println("a1.i : "+ a1.i + "a1.arr : "+ Arrays.toString(a1.arr));
+		System.out.println("a2.i : " + a2.i + "a2.arr : "+ Arrays.toString(a2.arr));
+		System.out.println("a3.i : " + a3.i + "a3.arr : "+ Arrays.toString(a3.arr));
+		
+		System.out.println("----------- shallo copy ------------");
+		a1.i = 9; a1.arr[0] = 99;
+		System.out.println("a1.i : "+ a1.i + "a1.arr : "+ Arrays.toString(a1.arr));
+		System.out.println("a2.i : " + a2.i + "a2.arr : "+ Arrays.toString(a2.arr));
+		
+		System.out.println("----------- deep copy ----------------");
+		System.out.println("a3.i : " + a3.i + "a3.arr : "+ Arrays.toString(a3.arr));
+		
 	}
 }
