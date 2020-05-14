e31b8bcac63cc47cee2921bf30e316098633f2cb
accidentlywoo <accidentlywoo@gmail.com>
2020-05-14 11:53:45 +0900

    제네릭이 없는 위험한 Collection으로 해보는 연습 : Map 인터페이스 구현체 HashMap

--- oop/src/com/my/api/CollectionTest.java
+++ oop/src/com/my/api/CollectionTest.java
@@ -2,8 +2,10 @@ package com.my.api;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class CollectionTest {
 	public static void test(Collection c) {
@@ -22,10 +24,32 @@ public class CollectionTest {
 		c.remove(new Integer(2)); // 요소 삭제 List 인터페이스 구현체는 index 순서로 삭제
 		System.out.println(c);
 	}
+	private static void test(HashMap m) {
+		m.put(new Integer(1), "first");
+		m.put(new Integer("2"), "second");
+		m.put(3, "third");
+		m.put(2, "fourth");
+		System.out.println("저장된 요소의 개수 : "+m.size());
+		System.out.println("------- 요소 조회하기 --------");
+		Set s = m.keySet(); // 전체 키값 반환
+		/*Iterator iter = s.iterator();
+		while(iter.hasNext()) {
+			Object k = iter.next();
+			Object v = m.get(k);
+			System.out.println(k + " = "+ v);
+		}*/
+		for(Object i : s) {
+			Object v = m.get(i);
+			System.out.println("key 값 : "+i + ", value 값 : "+ v);
+		}
+		m.remove(3);
+		System.out.println(m);
+	}
 	public static void main(String[] args) {
 		Collection c;
 		c = new ArrayList();
 		test(c);
 		test(new HashSet());
+		test(new HashMap());
 	}
 }
