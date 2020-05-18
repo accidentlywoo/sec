397e7ae588f3280de8e9576a38821fcf2dbf2bd2
accidentlywoo <accidentlywoo@gmail.com>
2020-05-18 10:26:51 +0900

    InputStreamReader로 한글깨짐없는 문자읽기

--- oop/src/com/my/io/KeyBoardTest.java
+++ oop/src/com/my/io/KeyBoardTest.java
@@ -3,16 +3,18 @@ package com.my.io;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class KeyBoardTest {
 	public static void main(String[] args) {
 		InputStream is = System.in;
+		Reader r =  new InputStreamReader(is);
 		try {
-			int readValue = is.read(); // Enter누를때까지 기둘
-			System.out.println(readValue + " : "+(char)readValue);
-			// byte 단위로 읽기
+			int readValue = r.read(); //char 일기
+			System.out.println(readValue + " : " + (char)readValue);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 }
