ee9f64a0f9fc1ebdeb102f2da94249167fc0acec
accidentlywoo <accidentlywoo@gmail.com>
2020-05-18 10:33:47 +0900

    배열단위로 문자 입력값 읽어오기

--- oop/src/com/my/io/KeyBoardTest.java
+++ oop/src/com/my/io/KeyBoardTest.java
@@ -9,11 +9,14 @@ public class KeyBoardTest {
 	public static void main(String[] args) {
 		InputStream is = System.in;
 		Reader r =  new InputStreamReader(is);
+		byte[] bytes = new byte[100];
 		try {
-			int readValue = r.read(); //char 일기
-			System.out.println(readValue + " : " + (char)readValue);
-		} catch (IOException e) {
-			e.printStackTrace();
+			int readCnt = is.read(bytes);
+			System.out.println("읽어온 바이트 수 : " + readCnt);
+			for(int i = 0; i < readCnt; i ++) {
+				System.out.println(bytes[i] + " : " + (char)bytes[i]);
+			}
+		}catch (IOException e) {
 		}
 		
 	}
