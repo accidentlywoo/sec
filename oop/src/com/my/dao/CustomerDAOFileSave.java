92f0b2bc8987f0cc1457cc818b3ab1d19416d70e
accidentlywoo <accidentlywoo@gmail.com>
2020-05-19 16:33:41 +0900

    기존 MVC 모델에서 파일 저장소 사용해보기 : 저장하기를 해야 파일에 저장되는 저장소

--- oop/src/com/my/dao/CustomerDAOFileSave.java
+++ oop/src/com/my/dao/CustomerDAOFileSave.java
@@ -1,5 +1,12 @@
 package com.my.dao;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +20,15 @@ import com.my.vo.Customer;
 public class CustomerDAOFileSave implements CustomerDAO2{
 	// 파일에 매번 수정하지 않고, [수정] 과정을 거치면 파일에 저장
 	private String fileName;
-	final private List<Customer> customers;
+	private List<Customer> customers = new ArrayList<Customer>();
 	 
 	public CustomerDAOFileSave() {
-		customers = new ArrayList<Customer>();
+		fileName = "customers.dat";
 	}
 
-	public void insert(Customer customer) throws AddException, DuplicatedException {
+	public void insert(Customer customer) throws AddException, DuplicatedException, FindException {
+		selectAll();
 		for(Customer c : customers) {
-//			if(c.getId().equals(customer.getId())) {
 			if(c.equals(customer)) {
 				throw new DuplicatedException("이미 존재하는 아이디입니다.");
 			}
@@ -30,26 +37,60 @@ public class CustomerDAOFileSave implements CustomerDAO2{
 	}
 
 	public List<Customer> selectAll() throws FindException {
-		if(customers.size() == 0) {
-			throw new FindException("등록된 고객이 없습니다.");
+		customers = new ArrayList<Customer>();
+		FileInputStream fis = null;
+		DataInputStream dis = null;
+		try {
+			fis = new FileInputStream(fileName);
+			dis = new DataInputStream(fis);
+			while(true) {
+				Customer customer = new Customer();
+				customer.setId(dis.readUTF());
+				customer.setPwd(dis.readUTF());
+				customer.setName(dis.readUTF());
+				customer.setAddr(dis.readUTF());
+				customers.add(customer);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (EOFException e) {
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(dis != null) {
+				try {
+					dis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		return customers;
 	}
 
 	public Customer selectById(String id) throws FindException {
-		for(Customer c : customers) {
-			if(c.getId().equals(id)) {
-				return c;
+		selectAll();
+		for(Customer customer : customers) {
+			if(customer.getId().equals(id)) {
+				return customer;
 			}
 		}
 		throw new FindException("해당 아이디의 고객을 찾을 수 없습니다");
 	}
 
 	public List<Customer> selectByName(String word) throws FindException {
+		selectAll();
 		List<Customer> result = new ArrayList<Customer>();
-		for(Customer c : customers) {
-			if(c.getName().contains(word)) {
-				result.add(c);
+		for(Customer customer : customers) {
+			if(customer.getName().contains(word)) {
+				result.add(customer);
 			}
 		}
 		if(result.size() == 0) {
@@ -58,23 +99,60 @@ public class CustomerDAOFileSave implements CustomerDAO2{
 		return result;
 	}
 
-	public void update(Customer customer) throws ModifyException {
-		try {
-			Customer c = selectById(customer.getId());
-			c.setId(customer.getId());
-			c.setPwd(customer.getPwd());
-			c.setName(customer.getName());
-			c.setAddr(customer.getAddr());
-		} catch (FindException e) {
-			throw new ModifyException(e.getMessage());
-		}
-		
+	public void update(Customer customer) throws ModifyException, FindException {
+		selectAll();
+		selectById(customer.getId());
+		customers.remove(customer);
+		Customer result = new Customer();
+		result.setId(customer.getId());
+		result.setPwd(customer.getPwd());
+		result.setName(customer.getName());
+		result.setAddr(customer.getId());
+		customers.add(result);
 	}
 	public void delete(String id) throws RemoveException {
 		Customer c = new Customer();
 		c.setId(id);
 		if(!customers.remove(c)) {
 			throw new RemoveException("해당 아이디를 삭제할 수 없습니다."); 
+		}
+	}
+	public void insertFile() {
+		try {
+			selectAll();
+		} catch (FindException e1) {
+			e1.printStackTrace();
+		}
+		FileOutputStream fos = null;
+		DataOutputStream dos = null;
+		try {
+			fos = new FileOutputStream(fileName);
+			dos = new DataOutputStream(fos);
+			for(Customer customer:customers) {
+				dos.writeUTF(customer.getId());
+				dos.writeUTF(customer.getPwd());
+				dos.writeUTF(customer.getName());
+				dos.writeUTF(customer.getAddr());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(dos != null) {
+				try {
+					dos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
