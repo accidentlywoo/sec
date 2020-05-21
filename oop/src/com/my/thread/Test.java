42cd9fb961ca69cff0b90edc8df836ebaf438b0d
accidentlywoo <accidentlywoo@gmail.com>
2020-05-21 12:46:42 +0900

    multi thread basic

--- oop/src/com/my/thread/Test.java
+++ oop/src/com/my/thread/Test.java
@@ -3,9 +3,10 @@ package com.my.thread;
 class First extends Thread{
 	public void run() {
 		for(int i = 1; i <= 100; i++) {
-			Thread currentThread = Thread.currentThread();
+			Thread currentThread = Thread.currentThread(); // run 상태의 객체 / 현재 Thread의 상태
 			String currtName = currentThread.getName();
 			System.out.println("currentThread Name : " + currtName+ " i = "+i);
+			System.out.println("Instance Name : " + this.getName()); //현재 객체의 이름
 		}
 	}
 }
