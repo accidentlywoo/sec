c87d9190c1679c4ee2c1adf8c9ad25fff1989d28
accidentlywoo <accidentlywoo@gmail.com>
2020-05-21 16:00:06 +0900

    multi Thread 한 쓰레드에 접근할때, 다른 결과같이 나올때! join()으로 접근 쓰레드가 죽기까지 기다린다.

--- oop/src/com/my/thread/ScheduleTest.java
+++ oop/src/com/my/thread/ScheduleTest.java
@@ -17,6 +17,16 @@ public class ScheduleTest {
 		one.start();
 		two.start();
 		
+		try {
+			one.join(); // main Thread가 one Thread가 죽기를 기다린다.
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		try {
+			two.join(); // main Thread가 one Thread가 죽기를 기다린다.
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		System.out.println(one.getTot()); // 결과값이 다르다!
 		System.out.println(one.getTot());
 		System.out.println(two.getTot());
