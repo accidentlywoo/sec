341437bf61f21e7016befff089b36fda2d021598
accidentlywoo <accidentlywoo@gmail.com>
2020-05-22 10:51:52 +0900

    Multi Thread로 보는 메소드 처리 흐름보기 : Synchronize Scope expention : Method Name 앞에 붙이는 synchronized 키워드 : 속도 저하가 될 수 있다

--- oop/src/com/my/thread/Share.java
+++ oop/src/com/my/thread/Share.java
@@ -2,17 +2,15 @@ package com.my.thread;
 
 public class Share {
 	int i;
-	public void inc() {
-		synchronized (this) {
-			for(int i = 1; i <= 100; i++) { 
-			// critical section 처리는? 
-			// -> 공유자원을 사용하는 다른 Thread에게 CPU를 뺏기지 않는 처리를 해줘야 한다. 
-			// -> 동기화 // synchronize
-			
-				System.out.println("before inc() this.i : " + this.i);
-				this.i = this.i +1;// -> 공유 자원
-				System.out.println("after inc() this.i = "+this.i);
-			}
+	synchronized public void inc() {
+		for(int i = 1; i <= 100; i++) { 
+		// critical section 처리는? 
+		// -> 공유자원을 사용하는 다른 Thread에게 CPU를 뺏기지 않는 처리를 해줘야 한다. 
+		// -> 동기화 // synchronize
+		
+			System.out.println("before inc() this.i : " + this.i);
+			this.i = this.i +1;// -> 공유 자원
+			System.out.println("after inc() this.i = "+this.i);
 		}
 	}
 	
