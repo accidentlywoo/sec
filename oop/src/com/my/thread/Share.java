54e2fd750c0e33723ab545c724ff4fe57177c43c
accidentlywoo <accidentlywoo@gmail.com>
2020-05-22 11:34:34 +0900

    Multi Thread wait()/notify()를 이용한 Thread간의 협업

--- oop/src/com/my/thread/Share.java
+++ oop/src/com/my/thread/Share.java
@@ -2,21 +2,27 @@ package com.my.thread;
 
 public class Share {
 	int i;
-	synchronized public void inc() {
+	public void inc() {
 		for(int i = 1; i <= 100; i++) { 
-		// critical section 처리는? 
-		// -> 공유자원을 사용하는 다른 Thread에게 CPU를 뺏기지 않는 처리를 해줘야 한다. 
-		// -> 동기화 // synchronize
-		
-			System.out.println("before inc() this.i : " + this.i);
-			this.i = this.i +1;// -> 공유 자원
-			System.out.println("after inc() this.i = "+this.i);
+			synchronized (this) {
+				this.notify();
+				System.out.println("before inc() this.i : " + this.i);
+				this.i = this.i +1;// -> 공유 자원
+				System.out.println("after inc() this.i = "+this.i);
+			}
 		}
 	}
 	
 	public void dec() {
 		for(int i = 1; i <= 100; i++) {
 			synchronized (this) {
+				if(this.i == 0){
+					try {
+						this.wait(); // Process가 끝나지 않는다.
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
 				System.out.println("before dec() this.i : " + this.i);
 				this.i = this.i -1;
 				System.out.println("after dec() this.i = "+this.i);
