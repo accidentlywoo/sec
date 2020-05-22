61777ba278991cbde1f960ad833a8464467772cb
accidentlywoo <accidentlywoo@gmail.com>
2020-05-22 10:19:03 +0900

    말달리는 Multi Thread : CPU가 알아서 경쟁하게 하자!

--- oop/src/com/my/gui/Horse.java
+++ oop/src/com/my/gui/Horse.java
@@ -23,9 +23,12 @@ public class Horse extends Canvas implements Runnable{ //paint(), repaint()
 	}
 	@Override
 	public void run() {
-		this.flag = true;
 		long millis = (long)(Math.random()*1000)+1;
+		this.flag = true;
 		while(flag) {
+			if(this.x>=340) {
+				this.flag = false;
+			}
 			try {
 				Thread.sleep(millis);
 			} catch (InterruptedException e) {
