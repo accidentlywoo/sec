705f88835c08020508d24f82652405dd381545da
accidentlywoo <accidentlywoo@gmail.com>
2020-05-25 11:08:29 +0900

    TCP DataI/OputStream을 이용하기

--- oop/src/com/my/net/TCPBufferedClient.java
+++ oop/src/com/my/net/TCPBufferedClient.java
@@ -1,37 +1,25 @@
 package com.my.net;
 
-import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 public class TCPBufferedClient {
 	public static void main(String[] args) {
 		int port = 6543;
-		String serverIp = "192.168.0.115";
+		String serverIp = "192.168.0.118";
 		Socket s = null;
-		OutputStream os = null;
-		BufferedWriter bw = null;
-//		Scanner sc = null;
+		DataOutputStream dos = null;
 		try {
 			s = new Socket(serverIp, port);
-			os = s.getOutputStream();
-			bw = new BufferedWriter(new OutputStreamWriter(os));
-			bw.write("id1:p1:n1:a1 \n"); bw.flush();
-			bw.write("id2:p2:n2:a2 \n"); bw.flush();
-			bw.write("id3:p3:n3:a3 \n"); bw.flush();
-			bw.write("quit \n"); bw.flush();
-//			String msg;
-//			do {
-//				System.out.println("서버로 보낼 메시지를 입력하세요. 종료하려면 quit을 입력하세요.");
-//				msg = sc.nextLine();
-//				bw.append(msg);
-//				bw.flush();
-//			}while(msg.equals("quit"));
+			dos = new DataOutputStream(s.getOutputStream());
+			for(int i = 0; i < 10; i++) {
+				dos.writeInt(i);
+				dos.writeBoolean(i%2 == 0 ? true : false);
+				dos.writeUTF("테스트" + i);
+			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}catch (ConnectException e) {
--- oop/src/com/my/net/TCPBufferedServer.java
+++ oop/src/com/my/net/TCPBufferedServer.java
@@ -1,36 +1,35 @@
 package com.my.net;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.net.SocketException;
 
 public class TCPBufferedServer {
 	public static void main(String[] args) {
 		int port = 6543;
 		ServerSocket ss = null;
 		Socket s = null;
-		BufferedReader br = null;
+		DataInputStream dis = null;
 		try {
 			ss = new ServerSocket(port);
 			s = ss.accept();
-			br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			String[] arr = null;
-			int flag = 0;
-			do {
-				String receive = br.readLine();
-				String delim = ":";
-				arr = receive.split(delim);
-				System.out.println("id = " +  arr[0] + ", name = "+arr[1]);
-				flag++;
-				if(receive.equals("quit")) {
-					break;
-				}
-			}while(arr.length ==flag);
+			
+			
+			dis = new DataInputStream(s.getInputStream());
+			
+			while(true) {
+				int receiveInt = dis.readInt();
+				boolean receiveBool = dis.readBoolean();
+				String receiveUTF = dis.readUTF();
+				System.out.println("클라이언트가 보낸 메세지 : " + receiveInt+", "+ receiveBool +", " +receiveUTF);
+			}
+		}catch (BindException e) {
+			System.out.println(port + "이미 사용중인 포트입니다.");
+		}catch (SocketException e) {
+			// 반복문이 계속되면서 발생
 		} catch (IOException e) {
 			e.printStackTrace();
 		}finally {
