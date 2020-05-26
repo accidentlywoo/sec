518746b997e82e22b18dc657ff3b8328c91285b2
accidentlywoo <accidentlywoo@gmail.com>
2020-05-26 09:17:42 +0900

    TCP Echo Multi Thread 1:N 구현하기 : 입력 받아서 Echo잉

---
+++ oop/src/com/my/net/TCPBroadcastEchoClient.java
@@ -0,0 +1,75 @@
+package com.my.net;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class TCPBroadcastEchoClient {
+	public static void main(String[] args) {
+		int port = 9876;
+		String serverIp = "192.168.0.115";
+		Socket s = null;
+		OutputStream os = null;
+		BufferedWriter bw = null;
+		BufferedReader br = null;
+		Scanner sc = null;
+		try {
+			
+			s = new Socket(serverIp, port);
+			os = s.getOutputStream();
+			bw = new BufferedWriter(new OutputStreamWriter(os));
+			br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			
+			sc = new Scanner(System.in);
+			
+			while(true) {
+				System.out.println("메시지를 입력하세요. : ");
+				String receive = sc.nextLine()+"\n";
+				bw.write(receive);
+				bw.flush();
+				if(receive.equals("quit")) {
+					break;
+				}
+				System.out.println(br.readLine());
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (ConnectException e) { // 연결되지 않을때
+			System.out.println();
+		} catch(SocketException e) { // 연결은됬으나 상대쪽에서 연결을 끊었을때.
+			System.out.println("서버와의 연결이 끊겼습니다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(bw != null) {
+				try {
+					bw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(s != null) {
+				try {
+					s.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
