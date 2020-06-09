9731527fd3eb17f3aa0fbdde7b5bc6c2a259ff2e
accidentlywoo <accidentlywoo@gmail.com>
2020-06-09 14:44:49 +0900

    Transaction / DeadLock:

--- db/주문실습.sql
+++ db/주문실습.sql
@@ -106,3 +106,63 @@ WHERE oi.order_id = 'id1'
 ORDER BY oi.order_no desc
 ;
 -- order_no order_id order_dt order_prod_no order_quantity
+
+-- 주문된 상품 목록을 출력하시오(상품번호, 상품명, 주문번호, 주문수량)
+-- 단, 주문되지 않은 상품도 출력한다.
+SELECT
+    p.prod_no
+    , p.prod_name
+    , ol.order_no
+    , ol.order_quantity
+FROM order_line ol
+    RIGHT OUTER JOIN product p
+    ON(ol.order_prod_no = p.prod_no)  
+;
+
+-- 상품별 주문수량의 합을 구하시오(상품번호, 주문수량 합)
+-- 가장 많이 판매된 상품순으로 출력
+SELECT   
+    p.prod_no
+    , SUM(ol.order_quantity)
+FROM order_line ol
+    RIGHT OUTER JOIN product p
+    ON(ol.order_prod_no = p.prod_no)
+    GROUP BY p.prod_no
+-- ORDER BY SUM(ol.order_quantity) DESC  
+ ORDER BY 2 DESC  
+;
+
+-- 상품별 주문수량의 합을 구하시오(상품번호, 주문수량 합)
+-- 가장 많이 판매된 상품순으로 출력
+-- 주문일자가 20/06/08인 주문중
+SELECT TO_CHAR(TO_DATE(order_dt), 'YY/MM/DD HH24:MI:SS')
+FROM order_info
+--WHERE order_dt >='20/06/08' AND order_dt <'20/06/09'
+WHERE order_dt = '20/06/08'
+;
+
+SELECT order_prod_no, SUM(order_quantity)
+FROM order_line
+WHERE order_no IN (SELECT order_no FROM order_info 
+                    WHERE order_dt = '20/06/08' )
+GROUP BY order_prod_no
+ORDER BY 2 DESC
+;
+
+-- 주문되지 않는 상품번호를 출력
+SELECT p.prod_no
+FROM product p
+MINUS
+SELECT order_prod_no
+FROM order_line
+;
+ -- 집합연산시 컬럼 갯수를 맞춰줘야 한다.
+SELECT *
+FROM product
+WHERE prod_no IN
+    (SELECT p.prod_no
+        FROM product p
+    MINUS
+    SELECT order_prod_no 
+        FROM order_line)
+;
