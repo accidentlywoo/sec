466009a231ae3821ef3ee821f70333b4aa2583c2
accidentlywoo <accidentlywoo@gmail.com>
2020-06-16 12:34:01 +0900

    우편번호 사이트에서 엑셀테이터 임폴트시키기, JDBC entity 생성

---
+++ db/우편번호.sql
@@ -0,0 +1,4 @@
+-- building 컬럼값에 '대학교'를 포함한 행들을 모두 출력하시오
+SELECT * FROM postal
+WHERE building like '%대학교%'
+;
--- jdbc/src/jdbc/MyConnectionTest.java
+++ jdbc/src/jdbc/MyConnectionTest.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import jdbc.com.my.sql.MyConnection;
+import com.my.sql.MyConnection;
 
 public class MyConnectionTest {
 	public static void main(String[] args) {
