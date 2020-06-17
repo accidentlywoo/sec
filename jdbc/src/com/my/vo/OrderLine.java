976bef1f631a7a5fe80e642953d51208a50b184b
accidentlywoo <accidentlywoo@gmail.com>
2020-06-17 12:24:49 +0900

    VO setting마무리, Product Entity hashcode()/equals() Override

--- jdbc/src/com/my/vo/OrderLine.java
+++ jdbc/src/com/my/vo/OrderLine.java
@@ -7,4 +7,30 @@ public class OrderLine {
 	private int order_quantity;
 	// Has A 관계를 잘 구현하면 Java Persistance 상태를 구현할 수 있다
 	// DB와 Java와의 영속적인 관계
+	
+	public OrderLine() {}
+	public OrderLine(int order_no, Product order_p, int order_quantity) {
+		this.order_no = order_no;
+		this.order_p = order_p;
+		this.order_quantity = order_quantity;
+	}
+	public int getOrder_no() {
+		return order_no;
+	}
+	public void setOrder_no(int order_no) {
+		this.order_no = order_no;
+	}
+	public Product getOrder_p() {
+		return order_p;
+	}
+	public void setOrder_p(Product order_p) {
+		this.order_p = order_p;
+	}
+	public int getOrder_quantity() {
+		return order_quantity;
+	}
+	public void setOrder_quantity(int order_quantity) {
+		this.order_quantity = order_quantity;
+	}
+	
 }
--- jdbc/src/com/my/vo/Product.java
+++ jdbc/src/com/my/vo/Product.java
@@ -1,5 +1,57 @@
 package com.my.vo;
 
 public class Product {
-
+	private String prod_no;
+	private String prod_name;
+	private String prof_price;
+	
+	
+	public Product() {}
+	public Product(String prod_no, String prod_name, String prof_price) {
+		this.prod_no = prod_no;
+		this.prod_name = prod_name;
+		this.prof_price = prof_price;
+	}
+	public String getProd_no() {
+		return prod_no;
+	}
+	public void setProd_no(String prod_no) {
+		this.prod_no = prod_no;
+	}
+	public String getProd_name() {
+		return prod_name;
+	}
+	public void setProd_name(String prod_name) {
+		this.prod_name = prod_name;
+	}
+	public String getProf_price() {
+		return prof_price;
+	}
+	public void setProf_price(String prof_price) {
+		this.prof_price = prof_price;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((prod_no == null) ? 0 : prod_no.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (prod_no == null) {
+			if (other.prod_no != null)
+				return false;
+		} else if (!prod_no.equals(other.prod_no))
+			return false;
+		return true;
+	}
+	
 }
